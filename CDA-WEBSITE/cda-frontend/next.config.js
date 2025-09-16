7722899156b79bda27fe3ea69e569a2f489d543e
Razvan Vlad Pop <razvanvladpop@gmail.com>
2025-09-16 12:50:07 +0300

    Comit next config

--- CDA-WEBSITE/cda-frontend/next.config.js
+++ CDA-WEBSITE/cda-frontend/next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   env: {
     // Prefer environment variables (Vercel or .env.*). Provide sensible defaults.
     // URL FOR LIVE SERVER select one or the other (working):
-    // NEXT_PUBLIC_WORDPRESS_URL: process.env.NEXT_PUBLIC_WORDPRESS_URL || 'https://cdanewwebsite.wpenginepowered.com',
+    NEXT_PUBLIC_WORDPRESS_URL: process.env.NEXT_PUBLIC_WORDPRESS_URL || 'https://cdanewwebsite.wpenginepowered.com',
     // URL FOR LIVE SERVER select one or the other (working):
-    NEXT_PUBLIC_WORDPRESS_URL: process.env.NEXT_PUBLIC_WORDPRESS_URL || 'http://localhost/CDA-WEBSITE-PROJECT/CDA-WEBSITE/wordpress-backend',
+    // NEXT_PUBLIC_WORDPRESS_URL: process.env.NEXT_PUBLIC_WORDPRESS_URL || 'http://localhost/CDA-WEBSITE-PROJECT/CDA-WEBSITE/wordpress-backend',
     // URL FOR LIVE SERVER select one or the other (working):
     NEXT_PUBLIC_WORDPRESS_GRAPHQL_ENDPOINT:
       process.env.NEXT_PUBLIC_WORDPRESS_GRAPHQL_ENDPOINT ||
