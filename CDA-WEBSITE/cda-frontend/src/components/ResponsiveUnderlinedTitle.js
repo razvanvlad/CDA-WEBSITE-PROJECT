cffa1743b58857597f00b509d09c10e2ec57a4bd
Razvan Vlad Pop <razvanvladpop@gmail.com>
2025-12-23 04:10:55 +0200

    Fixed build errors

--- CDA-WEBSITE/cda-frontend/src/components/ResponsiveUnderlinedTitle.js
+++ CDA-WEBSITE/cda-frontend/src/components/ResponsiveUnderlinedTitle.js
@@ -30,7 +30,7 @@ export default function ResponsiveUnderlinedTitle({
   children,
   className = '',
   underlineColor = '#FF5C8A',
-  compactUnderline,
+  compactUnderline = false,
   ...props
 }) {
   const [mounted, setMounted] = useState(false);
